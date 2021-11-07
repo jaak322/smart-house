f4b272d3f29d3a6983a710a2ca6f863a8f8bfb22
jaak322 <79160292+jaak322@users.noreply.github.com>
2021-11-07 23:31:14 +0100

    Update smart-house_python-script.py
    control lamp via RPi. GPIO interface

--- smart-house_python-script.py
+++ smart-house_python-script.py
@@ -2,7 +2,17 @@ from firebase import firebase
 import serial
 import time
 import re
+import RPi.GPIO as GPIO
+import time
 
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.OUT)
+
+
+time.sleep(5)
+
+GPIO.cleanup
 
 FBConn = firebase.FirebaseApplication('https://smart-house-ae2d9-default-rtdb.europe-west1.firebasedatabase.app/', None)
 
@@ -16,6 +26,7 @@ while True:
          
          line1 = line.decode('ASCII')
          line2 = line.decode('ASCII')
+         line3 = line.decode('ASCII')
          
          
          
@@ -25,24 +36,34 @@ while True:
          regex = '\d+[H]'
          match2 = re.findall(regex, line2)
          
+         regex = '\d+[L]'
+         match3 = re.findall(regex, line3)         
+         
          
          x = "".join(map(str, match1))
          y = "".join(map(str, match2))
+         z = "".join(map(str, match3))
          
          
          x1 =int(re.search(r'\d+', x).group(0))
          y1 =int(re.search(r'\d+', y).group(0))
+         z1 =int(re.search(r'\d+', z).group(0))
         
         
          
-         print(x1)
-         print(y1)
-         
+        
          
          line22 = FBConn.put("/Sensors","temperature",x1)
          line34 = FBConn.put("/Sensors","humidity",y1)
-       
+         line35 = FBConn.put("/Sensors","outdoor light",z1)
+         result = FBConn.get("/Lamps","lamp1",)
          
+         if (result ==1):
+              print(result)
+              GPIO.output(18, GPIO.HIGH)
+         elif (result ==0):
+              print(result)
+              GPIO.output(18, GPIO.LOW)
          
 
 
@@ -54,5 +75,6 @@ while True:
         
          
  
+
 
 
